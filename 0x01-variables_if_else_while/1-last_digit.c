4aed698d390ecd4c27c0e2438c2bb174e8b7ee71
Piusbassey <okonpius5@gmail.com>
2023-08-12 08:30:06 -0700

    last_digit

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -17,7 +17,7 @@ int main(void)
 	n = rand() - RAND_MAX / 2;
 	i = n % 10;
 
-	if (n > 5)
+	if (i > 5)
 	{
 	printf("Last digit of %d is %d and is greater than 5\n", n, i);
 	}
@@ -27,7 +27,7 @@ int main(void)
 	}
 	else
 	{
-	printf("Last digit of %d is %d and is less than 6 and not 0\n", n, i);
+	printf("Last digit of %d is 8 and is greater than 5 0\n", n, i);
 	}
 	return (0);
 }
