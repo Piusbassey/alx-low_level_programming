e6e537d1ef7d16785b4cb65155ce62cee286aa5b
Piusbassey <okonpius5@gmail.com>
2023-08-12 10:42:52 -0700

    alphabet

--- 0x01-variables_if_else_while/2-print_alphabet.c
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -6,32 +6,12 @@
  */
 int main(void)
 {
-	putchar('a');
-	putchar('b');
-	putchar('c');
-	putchar('d');
-	putchar('e');
-	putchar('f');
-	putchar('g');
-	putchar('h');
-	putchar('i');
-	putchar('j');
-	putchar('k');
-	putchar('l');
-	putchar('m');
-	putchar('n');
-	putchar('o');
-	putchar('p');
-	putchar('q');
-	putchar('r');
-	putchar('s');
-	putchar('t');
-	putchar('u');
-	putchar('v');
-	putchar('w');
-	putchar('x');
-	putchar('y');
-	putchar('z');
+	char alp;
+
+	for (alp = 'a'; alp <= 'z'; alp++)
+	{
+	putchar(alp);
+	}
 	putchar('\n');
 	return (0);
 }
