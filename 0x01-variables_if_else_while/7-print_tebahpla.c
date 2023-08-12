7ce2d5a2d8d994c4947fae8f33ab5baa0a6413fc
Piusbassey <okonpius5@gmail.com>
2023-08-12 13:01:19 -0700

    print_t

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * Return: Always (0)
+ i*/
+int main(void)
+{
+	char rev_alp;
+
+	for (rev_alp = 'z'; rev_alp >= 'a'; rev_alp--)
+	{
+		putchar(rev_alp);
+	}
+		putchar('\n');
+	return (0);
+}
