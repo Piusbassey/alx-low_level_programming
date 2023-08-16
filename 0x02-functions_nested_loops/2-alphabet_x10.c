18fa4418c18e5e453c27c3c998b0cb1e503163c3
Piusbassey <okonpius5@gmail.com>
2023-08-16 10:01:56 -0700

    alp

--- 0x02-functions_nested_loops/2-alphabet_x10.c
+++ 0x02-functions_nested_loops/2-alphabet_x10.c
@@ -1,11 +1,8 @@
 #include <stdio.h>
 #include "main.h"
-
 /**
- * print_alphabet_x10 - prints 10 times the alphabet
- *
- * Return: void (no return value)
- */
+  * print_alphabet_x10 - Prints the alphabet
+  */
 void print_alphabet_x10(void)
 {
 	char n;
