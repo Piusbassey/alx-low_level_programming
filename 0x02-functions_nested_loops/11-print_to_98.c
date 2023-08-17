1bc78a638cfdd54895459a7bacd320f5e7700a2e
Piusbassey <okonpius5@gmail.com>
2023-08-17 10:24:42 -0700

    pront

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -8,22 +8,29 @@
 void print_to_98(int n)
 {
 	int i;
-	if (n <= 98) {
-		for (i = n; i <= 98; i++) {
+
+	if (n <= 98)
+	{
+		for (i = n; i <= 98; i++)
+		{
 			printf("%d", i);
-			if (i != 98) {
+			if (i != 98)
+			{
 				putchar(',');
 				putchar(' ');
 			}
 		}
-	} else {
-		for (i = n; i >= 98; i--) {
-			printf("%d", i);
-			if (i != 98) {
-				putchar(',');
-				putchar(' ');
+		else
+		{
+			for (i = n; i >= 98; i--)
+			{
+				printf("%d", i);
+				if (i != 98)
+				{
+					putchar(',');
+					putchar(' ');
+				}
 			}
 		}
+		putchar('\n');
 	}
-	putchar('\n');
-}
