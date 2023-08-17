c59dc020cc5981c832e7fdbd01e509d9bcc03d15
Piusbassey <okonpius5@gmail.com>
2023-08-17 16:40:57 -0700

    debugging

--- 0x03-debugging/0-main.c
+++ 0x03-debugging/0-main.c
@@ -1,11 +1,16 @@
 #include <stdio.h>
 #include "main.h"
 
+/**
+ * main - Entry point.
+ * @i: Number to be printed.
+ * Return: Always (0) success.
+ */
 int main(void)
 {
-    int i = 0;
+	int i = 0;
 
-    positive_or_negative(i);
-
-    return (0);
+	i = 0;
+	positive_or_negative(i);
+	return (0);
 }
