2dd68d17521202a4c1e3752b8ac3b9d37081be29
Piusbassey <okonpius5@gmail.com>
2023-09-21 07:55:15 -0700

    advanced

---
+++ 0x12-singly_linked_lists/100-first.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+/**
+ * before_main - Function to print a message before main is executed.
+ */
+void before_main(void) __attribute__((constructor));
+
+/**
+ * before_main - Function to print a message before main is executed.
+ */
+void before_main(void)
+{
+	printf("You're beat! and yet, you must allow,\nI bore my house upon my back!\n");
+}
