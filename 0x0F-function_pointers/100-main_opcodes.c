f508a5f42b6b2a902ae215d83566f7e63396a42d
Piusbassey <okonpius5@gmail.com>
2023-11-08 20:23:35 +0000

    Write a program that prints the opcodes of its own main function.

---
+++ 0x0F-function_pointers/100-main_opcodes.c
@@ -0,0 +1,48 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * print_opcodes - Prints the opcodes of a given memory address
+ * @addr: The address of memory to print opcodes from
+ * @bytes: Number of bytes to print
+ */
+void print_opcodes(char *addr, int bytes)
+{
+	int i;
+
+	for (i = 0; i < bytes; i++)
+	{
+		printf("%02x", (unsigned char)addr[i]);
+		if (i < bytes - 1)
+			printf(" ");
+	}
+	printf("\n");
+}
+
+/**
+ * main - Prints opcodes of its own main function
+ * @argc: Argument count
+ * @argv: Argument vector
+ * Return: 0 on success, 1 for incorrect argument count, 2 for negative bytes
+ */
+int main(int argc, char *argv[])
+{
+	int num_bytes = atoi(argv[1]);
+	char *address = (char *)&main;
+
+	print_opcodes(address, num_bytes);
+
+	if (argc != 2)
+	{
+		printf("Error\n");
+		return (1);
+	}
+
+	if (num_bytes < 0)
+	{
+		printf("Error\n");
+		return (2);
+	}
+
+	return (0);
+}
