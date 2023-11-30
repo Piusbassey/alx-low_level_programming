d2829ee16bf9fc594c0ae5fe9633f0e2eee690cc
Piusbassey <okonpius5@gmail.com>
2023-11-30 15:57:51 +0000

    Write a function that reads a text file and prints it to the POSIX standard output.

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -17,12 +17,10 @@ ssize_t read_textfile(const char *filename, size_t letters)
 
 	if (filename == NULL)
 		return (0);
-
 	fd = open(filename, O_RDONLY);
 
 	if (fd == -1)
 		return (0);
-
 	buffer = malloc(letters);
 
 	if (buffer == NULL)
@@ -30,25 +28,19 @@ ssize_t read_textfile(const char *filename, size_t letters)
 		close(fd);
 		return (0);
 	}
-
 	memset(buffer, 0, letters);
-
 	bytesRead = read(fd, buffer, letters);
 
 	close(fd);
-
 	if (bytesRead == -1)
 	{
 		free(buffer);
 		return (0);
 	}
-
 	bytesWritten = write(STDOUT_FILENO, buffer, bytesRead);
 
 	free(buffer);
-
 	if (bytesWritten == -1 || (size_t)bytesWritten != (size_t)bytesRead)
 		return (0);
-
 	return (bytesRead);
 }
